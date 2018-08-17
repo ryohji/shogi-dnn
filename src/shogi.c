3f2f53c17f1f245207074194f65798bdb1158654
Ikebe, Ryohji <Ryohji.Ikebe@gmail.com>
2018-08-17 11:17:33 +0900

    Add inversion helper functions.

--- src/shogi.c
+++ src/shogi.c
@@ -7,6 +7,9 @@
 
 static void reorder_captured(struct board *b);
 
+static enum cell inverted_cell(enum cell c);
+static enum captured inverted_captured(enum captured c);
+
 struct board board_new() {
     const struct board board = { {
         CELL_W_KYO, CELL_W_KEI, CELL_W_GIN, CELL_W_KIN, CELL_W_GYOKU, CELL_W_KIN, CELL_W_GIN, CELL_W_KEI, CELL_W_KYO,
@@ -110,6 +113,60 @@ void reorder_captured(struct board *b) {
 
 int compar_captured(const void *a, const void *b) {
     return *(const enum captured*)a - *(const enum captured*)b;
+}
+
+enum cell inverted_cell(enum cell c) {
+    switch (c) {
+    case CELL_B_GYOKU:   return CELL_W_GYOKU;
+    case CELL_B_RYU:     return CELL_W_RYU;
+    case CELL_B_HISHA:   return CELL_W_HISHA;
+    case CELL_B_UMA:     return CELL_W_UMA;
+    case CELL_B_KAKU:    return CELL_W_KAKU;
+    case CELL_B_KIN:     return CELL_W_KIN;
+    case CELL_B_NARIGIN: return CELL_W_NARIGIN;
+    case CELL_B_GIN:     return CELL_W_GIN;
+    case CELL_B_NARIKEI: return CELL_W_NARIKEI;
+    case CELL_B_KEI:     return CELL_W_KEI;
+    case CELL_B_NARIKYO: return CELL_W_NARIKYO;
+    case CELL_B_KYO:     return CELL_W_KYO;
+    case CELL_B_TO:      return CELL_W_TO;
+    case CELL_B_FU:      return CELL_W_FU;
+    case CELL_BLANK:     return CELL_BLANK;
+    case CELL_W_FU:      return CELL_B_FU;
+    case CELL_W_TO:      return CELL_B_TO;
+    case CELL_W_KYO:     return CELL_B_KYO;
+    case CELL_W_NARIKYO: return CELL_B_NARIKYO;
+    case CELL_W_KEI:     return CELL_B_KEI;
+    case CELL_W_NARIKEI: return CELL_B_NARIKEI;
+    case CELL_W_GIN:     return CELL_B_GIN;
+    case CELL_W_NARIGIN: return CELL_B_NARIGIN;
+    case CELL_W_KIN:     return CELL_B_KIN;
+    case CELL_W_KAKU:    return CELL_B_KAKU;
+    case CELL_W_UMA:     return CELL_B_UMA;
+    case CELL_W_HISHA:   return CELL_B_HISHA;
+    case CELL_W_RYU:     return CELL_B_RYU;
+    case CELL_W_GYOKU:   return CELL_B_GYOKU;
+    }
+}
+
+enum captured inverted_captured(enum captured c) {
+    switch (c) {
+    case CAPT_B_HISHA: return CAPT_W_HISHA;
+    case CAPT_B_KAKU:  return CAPT_W_KAKU;
+    case CAPT_B_KIN:   return CAPT_W_KIN;
+    case CAPT_B_GIN:   return CAPT_W_GIN;
+    case CAPT_B_KEI:   return CAPT_W_KEI;
+    case CAPT_B_KYO:   return CAPT_W_KYO;
+    case CAPT_B_FU:    return CAPT_W_FU;
+    case CAPT_BLANK:   return CAPT_BLANK;
+    case CAPT_W_FU:    return CAPT_B_FU;
+    case CAPT_W_KYO:   return CAPT_B_KYO;
+    case CAPT_W_KEI:   return CAPT_B_KEI;
+    case CAPT_W_GIN:   return CAPT_B_GIN;
+    case CAPT_W_KIN:   return CAPT_B_KIN;
+    case CAPT_W_KAKU:  return CAPT_B_KAKU;
+    case CAPT_W_HISHA: return CAPT_B_HISHA;
+    }
 }
 
 #ifdef UNITTEST_
